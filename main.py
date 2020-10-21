96f33dd89c496b02563ff353aa22a750a05299e8
Posfay Benedek <benedek@posfay.com>
2020-10-21 18:27:06 +0200

    added executable and counter

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
 import sys
 import zero_forcing_process as zf
 import graph_utils
 
 
 nodes = int(sys.argv[1])
+n = 0
 
 while True:
+    n += 1
     graph, initial_black_nodes_list = zf.generate_initial_coloring(nodes)
     zf_number, init_black_nodes_successful = zf.simulate_zero_forcing_on_graph(graph, initial_black_nodes_list)
     graph_utils.write_graph_to_file(graph, zf_number, init_black_nodes_successful)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(n)
